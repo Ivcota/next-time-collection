1bb6798017a576b7cf6e005ed2c8fd79e1324bf5
Ivcota <ivcotad@gmail.com>
2022-02-09 17:03:14 -0700

    pwa support

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+  },
+});
+module.exports = nextConfig;
